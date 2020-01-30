f886f33bdf3a15ff4e36697c9d19f8b9dc53d0d8
Kennice Ken <kennice_offor@gmail.com>
2020-01-30 22:52:02 +0800

    Project Setup Template

---
+++ src/components/shared/TheFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        <strong>Olingo</strong> by <a href="http://www.kwebdeveloper.com/portfolio">Kennice Ken</a>. Copyright © 2020
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style scoped>
+</style>
